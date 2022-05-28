b33e32eda4c45989dd034fd9d3f068063bb592de
Lattice 0 <lattice0@protonmail.com>
2022-05-28 23:52:44 +0000

    some stuff

--- firecracker_build.sh
+++ firecracker_build.sh
@@ -1,3 +1,7 @@
 #(cd firecracker && cargo build --target aarch64-linux-android)
 #(cd firecracker && cargo build --target x86_64-unknown-linux-gnu)
 (cd firecracker/ && cargo ndk -t arm64-v8a build)
+(adb push firecracker/build/cargo_target/aarch64-linux-android/debug/firecracker /sdcard && \
+adb shell su -c "cp /sdcard/firecracker /data/local" && \
+adb shell su -c "chmod +x /data/local/firecracker" && \
+adb shell su -c "/data/local/firecracker --help")
